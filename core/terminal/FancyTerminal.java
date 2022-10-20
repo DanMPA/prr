5489830b349f10f36ee448825b4f4e4151168c4e
Mafalda Botelho <mafaldacunhabotelho@hotmail.com>
2022-10-20 12:01:15 +0100

    ~

--- core/terminal/FancyTerminal.java
+++ core/terminal/FancyTerminal.java
@@ -3,11 +3,17 @@ package prr.core.terminal;
 import prr.core.client.Client;
 
 public class FancyTerminal extends Terminal {
-
+	
 	public FancyTerminal(String _id, Client _owner) {
 		super(_id, _owner);
 	}
 
+	/**
+	 * Converts Terminal Object to a String representation
+	 * @return String in the format
+	 * 			terminalType|terminalId|clientId|terminalStatus|balance-paid|balance-debts|friend1,...,friend
+	 *			terminalType|terminalId|clientId|terminalStatus|balance-paid|balance-debts
+	 */
 	@Override
 	public String toString() {
 		String temp = super.toString();
