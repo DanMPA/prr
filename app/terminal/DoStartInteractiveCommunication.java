cb9bde940103110614a78e2c218405f3e62f7414
Daniel Martins <danielmpa22@gmail.com>
2022-11-02 12:43:14 +0000

    Added start interactive communication

--- app/terminal/DoStartInteractiveCommunication.java
+++ app/terminal/DoStartInteractiveCommunication.java
@@ -1,23 +1,41 @@
 package prr.app.terminal;
 
 import prr.core.Network;
+import prr.core.exception.UnavailableEntity;
+import prr.core.exception.UnknownKeyException;
 import prr.core.terminal.Terminal;
+import prr.core.terminal.TerminalMode;
+import java.util.Objects;
 import prr.app.exception.UnknownTerminalKeyException;
 import pt.tecnico.uilib.forms.Form;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
 
 /**
  * Command for starting communication.
  */
 class DoStartInteractiveCommunication extends TerminalCommand {
 
-  DoStartInteractiveCommunication(Network context, Terminal terminal) {
-    super(Label.START_INTERACTIVE_COMMUNICATION, context, terminal, receiver -> receiver.canStartCommunication());
-  }
-  
-  @Override
-  protected final void execute() throws CommandException {
-    //FIXME implement command
-  }
+	DoStartInteractiveCommunication(Network context, Terminal terminal) {
+		super(Label.START_INTERACTIVE_COMMUNICATION, context, terminal,
+				Terminal::canStartCommunication);
+		addStringField("destinationId", Message.terminalKey());
+	}
+
+	@Override
+	protected final void execute() throws CommandException {
+		String destinationId = stringField("destinationId");
+		try {
+			_network.generateInteractiveCommunication(_receiver, destinationId,
+					Form.requestString(Message.commType()));
+		} catch (UnknownKeyException e) {
+			throw new UnknownTerminalKeyException(destinationId);
+		} catch (UnavailableEntity e) {
+			if(e.getMode() == TerminalMode.SILENT){
+				Message.destinationIsSilent(destinationId);
+			} else if (e.getMode() == TerminalMode.BUSY){
+				Message.destinationIsBusy(destinationId);
+			}
+		}
+
+	}
 }
