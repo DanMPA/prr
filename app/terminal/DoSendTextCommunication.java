806eff91aab6962347d262c11a555606225345e6
Daniel Martins <danielmpa22@gmail.com>
2022-11-02 12:42:33 +0000

    removed an unused import

--- app/terminal/DoSendTextCommunication.java
+++ app/terminal/DoSendTextCommunication.java
@@ -5,7 +5,6 @@ import prr.core.exception.UnavailableEntity;
 import prr.core.exception.UnknownKeyException;
 import prr.core.terminal.Terminal;
 import prr.app.exception.UnknownTerminalKeyException;
-import pt.tecnico.uilib.forms.Form;
 import pt.tecnico.uilib.menus.CommandException;
 
 //FIXME add more imports if needed
