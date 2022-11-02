6dd5234ad23708ecf2863d986551081306f90a33
Daniel Martins <danielmpa22@gmail.com>
2022-11-02 12:44:57 +0000

    added Terminal mode as a valid Exception

--- core/exception/UnavailableEntity.java
+++ core/exception/UnavailableEntity.java
@@ -1,9 +1,17 @@
 package prr.core.exception;
 
+import prr.core.terminal.TerminalMode;
 public class UnavailableEntity extends Exception{
+	private TerminalMode  _mode;
 
-    public UnavailableEntity(String message) {
-        super(message);
+	public UnavailableEntity(String destination) {
+        super(destination);
     }
+	public UnavailableEntity(TerminalMode mode) {
+        _mode = mode;
+    }
+	public TerminalMode getMode(){
+		return this._mode;
+	}
 
 }
