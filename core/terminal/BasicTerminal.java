b182a750b21cb469312c403d7e052685c99319ed
Daniel Martins <danielmpa22@gmail.com>
2022-11-03 20:42:04 +0000

    Comments

--- core/terminal/BasicTerminal.java
+++ core/terminal/BasicTerminal.java
@@ -5,28 +5,41 @@ import prr.core.communication.Communication;
 import prr.core.exception.UnsupportedCommunicationExceptionDestination;
 import prr.core.exception.UnsupportedCommunicationExceptionOrigin;
 
-
-public class BasicTerminal extends Terminal{
+public class BasicTerminal extends Terminal {
 
 	public BasicTerminal(String id, Client owner) {
 		super(id, owner);
 	}
 
+	/**
+	 * Cant make video communication throw an exception.
+	 * 
+	 * @param destination The destination terminal.
+	 * @throws UnsupportedCommunicationExceptionOrigin
+	 */
 	@Override
-	public Communication makeVideoCommunication(Terminal destination) throws UnsupportedCommunicationExceptionOrigin {
+	public Communication makeVideoCommunication(Terminal destination)
+			throws UnsupportedCommunicationExceptionOrigin {
 		throw new UnsupportedCommunicationExceptionOrigin("VIDEO");
 	}
-	
+
+	/**
+	 * If the destination cant receive video communication throw an exception.
+	 * 
+	 * @throws UnsupportedCommunicationExceptionDestination
+	 */
 	@Override
-	public boolean canReciveVideoCommunication()throws UnsupportedCommunicationExceptionDestination {
-		throw new UnsupportedCommunicationExceptionDestination("VIDEO"); 
+	public boolean canReciveVideoCommunication()
+			throws UnsupportedCommunicationExceptionDestination {
+		throw new UnsupportedCommunicationExceptionDestination("VIDEO");
 	}
 
 	/**
 	 * Converts Terminal Object to a String representation
+	 * 
 	 * @return String in the format
-	 * 			terminalType|terminalId|clientId|terminalStatus|balance-paid|balance-debts|friend1,...,friend
-	 *			terminalType|terminalId|clientId|terminalStatus|balance-paid|balance-debts
+	 *         terminalType|terminalId|clientId|terminalStatus|balance-paid|balance-debts|friend1,...,friend
+	 *         terminalType|terminalId|clientId|terminalStatus|balance-paid|balance-debts
 	 */
 	@Override
 	public String toString() {
